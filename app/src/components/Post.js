59cc74451bd07e56d71286882a19a838c08ed506
Hokuto Handa <hokuto_1121@icloud.com>
2020-04-13 10:19:14 +0900

    fixed post

--- app/src/components/Post.js
+++ app/src/components/Post.js
@@ -1,28 +1,25 @@
-import React, { Component, useCallback } from 'react';
-import { connect } from 'react-redux'
-import { Field, reduxForm } from 'redux-form';
-// import Dropzone from 'react-dropzone'
+import React, { useCallback } from 'react';
+import { useDispatch } from 'react-redux';
+import { Form, Field } from 'react-final-form';
 import {useDropzone} from 'react-dropzone'
-import { Link } from 'react-router-dom';
+import { Link, useHistory } from 'react-router-dom';
 import { postIt } from '../actions';
 
 let formData = new FormData();
 let file = [];
 
 function Dropzone() {
-  //params へのデータ追加はSUBMITにしなければならない
+  //params へのデータ追加はSUBMIT内でしないと反映されない
   const onDrop = useCallback(acceptedFiles => {
     // params.append("image", acceptedFiles[0]);←無効
     file = acceptedFiles;
   }, [])
-
   const {getRootProps, getInputProps, isDragActive, acceptedFiles} = useDropzone({ onDrop, accept: 'image/png', maxSize: 1024*1024})
   const files = acceptedFiles.map(file => (
     <li key={file.path}>
       {file.path} - {Math.ceil(file.size/1024)} kb
     </li>
   ));
-
   return (
     <section>
       <div {...getRootProps({className:"dropzone"})}>
@@ -40,6 +37,41 @@ function Dropzone() {
   )
 }
 
+function AnimalForm() {
+  const dispatch = useDispatch();
+  let history = useHistory();
+  const onSubmit = async(values) => {
+    formData.append("type", "post");
+    formData.append("age", values.age);
+    formData.append("name", values.name);
+    formData.append("image", file[0]);
+    await dispatch(postIt(formData));
+    //次のpostに備えて初期化
+    formData = new FormData();
+    history.push('/');
+    file = [];
+  }
+  return(
+    <Form
+      onSubmit={onSubmit}
+      render={({ handleSubmit }) => (
+        <form onSubmit={handleSubmit}>
+          <div>
+            <label htmlFor="name">Name</label>
+            <Field name="name" component="input" type="text" />
+          </div>
+          <div>
+            <label htmlFor="age">Age</label>
+            <Field name="age" component="input" type="text" />
+          </div>
+          <Dropzone />
+          <button type="submit">Submit</button>
+        </form>
+      )}
+     />
+    );
+}
+
 function LinkArea() {
   return(
     <div>
@@ -48,64 +80,20 @@ function LinkArea() {
   );
 }
 
-class Post extends Component {
-  constructor(props){
-    super(props);
-    this.postSubmit = this.postSubmit.bind(this);
-    // this.renderFormArea = this.renderFormArea.bind(this);
-  }
-  async postSubmit(values){
-    const { postIt, history } = this.props;
-    formData.append("type", "post");
-    formData.append("age", values.age);
-    formData.append("name", values.name);
-    formData.append("image", file[0]);
-    await postIt(formData);
-    //次のpostに備えて初期化
-    formData = new FormData();
-    history.push('/');
-    file = [];
-  }
-  renderFormArea(){
-    const {handleSubmit} = this.props;
-    return(
-      <form onSubmit={handleSubmit(this.postSubmit)}>
-        <div>
-          <label htmlFor="name">Name</label>
-          <Field name="name" component="input" type="text" />
-        </div>
-        <div>
-          <label htmlFor="age">Age</label>
-          <Field name="age" component="input" type="text" />
-        </div>
-        <Dropzone />
-        <button type="submit">Submit</button>
-      </form>
-    );
-  }
-  render(){
-    return(
-      <div>
-        <header>
-          <h1>this is post</h1>
-        </header>
-        <main>
-          {this.renderFormArea()}
-        </main>
-        <footer>
-        <LinkArea/>
-        </footer>
-      </div>
-    );
-  }
+function Post() {
+  return(
+    <div>
+      <header>
+        <h1>this is post</h1>
+      </header>
+      <main>
+        <AnimalForm />
+      </main>
+      <footer>
+      <LinkArea/>
+      </footer>
+    </div>
+  );
 }
 
-const mapStateToProps = (state) => {
-  return (
-  {}
-)};
-const mapDispatchToProps = (dispatch) => ({
-  postIt: (formData)=> dispatch(postIt(formData)),
-});
-
-export default connect(mapStateToProps, mapDispatchToProps)(reduxForm({form:'postman'})(Post));
+export default Post;
