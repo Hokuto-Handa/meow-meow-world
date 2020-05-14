733f77b6e5c05fa605112ffd81dd89e741ac9d05
Hokuto Handa <hokuto_1121@icloud.com>
2020-05-14 16:42:24 +0900

    url 修正

--- app/src/actions/index.js
+++ app/src/actions/index.js
@@ -4,12 +4,12 @@ export const GET = 'GET';
 export const POST = 'POST';
 
 export const get = () => async (dispatch) => {
-  const animals = await axios.get('http://ec2-54-199-94-222.ap-northeast-1.compute.amazonaws.com:8080');
+  const animals = await axios.get('http://ec2-52-69-226-76.ap-northeast-1.compute.amazonaws.com:8080');
   dispatch({type: GET, animals});
 }
 
 export const postIt = (formData) => async (dispatch) => {
-  const response = await axios.post("http://ec2-54-199-94-222.ap-northeast-1.compute.amazonaws.com:8080", formData);
+  const response = await axios.post("http://ec2-52-69-226-76.ap-northeast-1.compute.amazonaws.com:8080", formData);
   console.log(response);
   dispatch({ type: POST, });
 };
