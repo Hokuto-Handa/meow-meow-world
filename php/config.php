c09033482189a9feb210f7a7be46d148c877d0c4
Hokuto Handa <hokuto_1121@icloud.com>
2020-05-14 16:04:24 +0900

    誤りの修正

---
+++ php/config.php
@@ -0,0 +1,17 @@
+<?php
+  //aws
+  // define('DNS', 'mysql:host=tutorial-db-instance.cqfk21srndys.ap-northeast-1.rds.amazonaws.com;dbname=database1');
+  // define('USER', 'tutorial_user');
+  // define('PASS', 'ricecake2');
+
+  //aws2nd
+  define('DNS', 'mysql:host=meow-db.cqfk21srndys.ap-northeast-1.rds.amazonaws.com;dbname=database1');
+  define('USER', 'meow_user');
+  define('PASS', 'meowmeow3841');
+
+  //local
+  // define('DNS', 'mysql:host=mysql;dbname=database1');
+  // define('USER', 'user');
+  // define('PASS', 'userpass');
+
+?>
