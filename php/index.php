060b48cf18c4d547dcc73121e49eba0a748e34ae
Hokuto Handa <hokuto_1121@icloud.com>
2020-05-15 15:03:10 +0900

    データベースリセット用

--- php/index.php
+++ php/index.php
@@ -2,8 +2,7 @@
 
 require_once(__DIR__ . '/config.php');
 header('Content-type:application/json; cahrset=UTF-8');
-header("Access-Control-Allow-Origin: *");
-// header("Access-Control-Allow-Origin: http://localhost:3000");
+header("Access-Control-Allow-Origin: http://ec2-13-114-28-204.ap-northeast-1.compute.amazonaws.com");
 
 class DataBase{
   private $_pdo;
@@ -19,9 +18,13 @@ class DataBase{
   }
 
   public function initDB(){
+    $this->_pdo->exec('drop table animals');
     $this->_pdo->exec('create table animals (id int auto_increment primary key,name varchar(255),age int,image varchar(255) default "no_image.png");');
     $this->_pdo->exec('insert into animals (name, age, image) values ("にゃんこ", 4, "cat.png");');
-    $this->_pdo->exec('insert into animals (name, age, image) values ("うさ", 2, "rabbit.png");');
+    $this->_pdo->exec('insert into animals (name, age, image) values ("うさ太郎", 2, "rabbit.png");');
+    $this->_pdo->exec('insert into animals (name, age, image) values ("ふくすけ", 2, "fuku.png");');
+    $this->_pdo->exec('insert into animals (name, age, image) values ("みゃあにゃん", 4, "cat2.png");');
+    $this->_pdo->exec('insert into animals (name, age, image) values ("うさうさ", 2, "rabbit2.png");');
   }
   public function getData(){
     $stmt = $this->_pdo->query("select * from animals");
